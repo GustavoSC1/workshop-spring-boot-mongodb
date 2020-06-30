73a6d665e8e8072502ba8019191425d7dfa901a5
Gustavo Cruz <gu.cruz17@hotmail.com>
2020-06-30 17:47:37 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/gustavo/workshopmongo/domain/User.java
+++ src/main/java/com/gustavo/workshopmongo/domain/User.java
@@ -2,9 +2,15 @@ package com.gustavo.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+// @Document e @Id indicam que se trata de uma coleção do MongoDB
+@Document(collection="user")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;

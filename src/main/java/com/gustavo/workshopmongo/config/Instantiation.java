705a3632de17e9326c95186446cc5180f1984a4e
Gustavo Cruz <gu.cruz17@hotmail.com>
2020-07-03 17:19:44 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/gustavo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustavo/workshopmongo/config/Instantiation.java
@@ -12,6 +12,7 @@ import com.gustavo.workshopmongo.domain.Post;
 import com.gustavo.workshopmongo.domain.User;
 import com.gustavo.workshopmongo.repository.PostRepository;
 import com.gustavo.workshopmongo.repository.UserRepository;
+import com.gustavo.workshopmongo.dto.AuthorDTO;;
 
 @Configuration
 //Executa partes específicas de código quando um aplicativo é iniciado. CommandLineRunner é uma interface usada para indicar que 
@@ -38,10 +39,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
+		//É preciso salvar os usuarios primeiro, para que eles tenham um id próprio, e depois associal os autor com o post.		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+				
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		

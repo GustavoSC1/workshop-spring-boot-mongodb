ea5a58b622d6d4b3aa7cd5df803586268e9bb692
Gustavo Cruz <gu.cruz17@hotmail.com>
2020-07-06 19:41:14 -0300

    Consulta simples com query methods

--- src/main/java/com/gustavo/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/gustavo/workshopmongo/repository/PostRepository.java
@@ -1,10 +1,15 @@
 package com.gustavo.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.gustavo.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	//https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#repositories.query-methods
+	List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
